8b3ccbfb628b35754fe85403cfee2db6d1f3006f
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-01-04 16:02:37 +0600

    Connected server and DB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+const connect = () => {
+	mongoose
+		.connect(process.env.MONGO_URL)
+		.then(() => {
+			console.log('Connected to DB');
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
+
+app.listen(5000, () => {
+	connect();
+	console.log('Connected to server!');
+});
