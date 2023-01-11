fcb2729d7167f5f42578cd4449ddc67d1fff7df6
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-01-11 15:20:11 +0600

    Update User API Done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
-import { createError } from './../error.js';
+
 import jwt from 'jsonwebtoken';
+import { createError } from './../error.js';
 
 export const signup = async (req, res, next) => {
 	// console.log(req.body);
@@ -13,8 +14,7 @@ export const signup = async (req, res, next) => {
 		await newUser.save();
 		res.status(200).send('User has been created!');
 	} catch (err) {
-		// next(createError('404', 'not found sorry!'));
-		next(err);
+		next(createError('404', 'not found sorry!'));
 	}
 };
 export const signin = async (req, res, next) => {
@@ -28,14 +28,15 @@ export const signin = async (req, res, next) => {
 			user.password
 		);
 		if (!isCorrect) return next(createError(400, 'Wrong Credentials!'));
+
 		const token = jwt.sign({ id: user._id }, process.env.JWT_SEC_KEY);
+		const { password, ...others } = user._doc;
 		res.cookie('access_token', token, {
 			httpOnly: true,
 		})
 			.status(200)
-			.json(user);
+			.json(others);
 	} catch (err) {
-		// next(createError('404', 'not found sorry!'));
-		next(err);
+		next(createError('404', 'not found sorry!'));
 	}
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,28 @@
-export const test = (req, res) => {
-	res.json("it's Successful!");
+import User from '../models/User.js';
+import { createError } from '../error.js';
+
+export const update = async (req, res, next) => {
+	if (req.params.id === req.user.id) {
+		try {
+			const updatedUser = await User.findByIdAndUpdate(
+				req.params.id,
+				{
+					$set: req.body,
+				},
+				{ new: true }
+			);
+			res.status(200).json(updatedUser);
+		} catch (err) {
+			next(err);
+		}
+	} else {
+		return next(createError(403, 'You can update only your account!'));
+	}
 };
+
+export const deleteUser = (req, res, next) => {};
+export const getUser = (req, res, next) => {};
+export const subscribe = (req, res, next) => {};
+export const unsubscribe = (req, res, next) => {};
+export const like = (req, res, next) => {};
+export const dislike = (req, res, next) => {};
