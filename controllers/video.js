0904cc65fed551e79cefc1d057ed1e6bb16e67ee
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-01-16 15:45:33 +0600

    Backend Done

--- controllers/video.js
+++ controllers/video.js
@@ -107,7 +107,6 @@ export const sub = async (req, res, next) => {
 
 export const getByTag = async (req, res, next) => {
 	const tags = req.query.tags.split(',');
-	// console.log(tags)
 	try {
 		const videos = await Video.find({ tags: { $in: tags } }).limit(20);
 		res.status(200).json(videos);
